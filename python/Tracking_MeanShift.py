748a05cfa75b0646b1a7a34862570d885e658506
angpre <angelo02prete@gmail.com>
2026-01-28 10:19:51 +0100

    Added helper functions

--- python/Tracking_MeanShift.py
+++ python/Tracking_MeanShift.py
@@ -1,25 +1,32 @@
 import numpy as np
 import cv2
+import os
+
+EXPERIMENT_NAME = 'Ball_HS'
+EXPERIMENT_PATH = os.path.join('../Experiments', EXPERIMENT_NAME)
+if not os.path.exists(EXPERIMENT_PATH):
+    os.makedirs(EXPERIMENT_PATH)
+CHANNELS = "HS"
 
 roi_defined = False
  
 def define_ROI(event, x, y, flags, param):
-	global r,c,w,h,roi_defined
-	# if the left mouse button was clicked, 
-	# record the starting ROI coordinates 
-	if event == cv2.EVENT_LBUTTONDOWN:
-		r, c = x, y
-		roi_defined = False
-	# if the left mouse button was released,
-	# record the ROI coordinates and dimensions
-	elif event == cv2.EVENT_LBUTTONUP:
-		r2, c2 = x, y
-		h = abs(r2-r)
-		w = abs(c2-c)
-		r = min(r,r2)
-		c = min(c,c2)  
-		roi_defined = True
-		
+    global r,c,w,h,roi_defined
+    # if the left mouse button was clicked, 
+    # record the starting ROI coordinates 
+    if event == cv2.EVENT_LBUTTONDOWN:
+        r, c = x, y
+        roi_defined = False
+    # if the left mouse button was released,
+    # record the ROI coordinates and dimensions
+    elif event == cv2.EVENT_LBUTTONUP:
+        r2, c2 = x, y
+        h = abs(r2-r)
+        w = abs(c2-c)
+        r = min(r,r2)
+        c = min(c,c2)  
+        roi_defined = True
+        
 def show_channels_and_weigths(hsv, weights):
     hue = hsv.copy()
     hsv2 = np.full(frame.shape[:2], 255)
@@ -33,9 +40,56 @@ def show_channels_and_weigths(hsv, weights):
     
     val = hsv[:,:,2]
     cv2.imshow("Value", val)
-	
+    
     cv2.imshow("Weights", weights)
     return hue_rgb, sat, val, weights
+
+def normalize_bgr_frame(frame: cv2.typing.MatLike) -> cv2.typing.MatLike:
+    """Given a frame, returns a new one where at each pixel we apply (B,G,R) = (B,G,R) / (B + G + R)"""
+    f = frame.astype(np.float32)
+    b, g, r = cv2.split(f)
+
+    channel_sum = b + g + r
+    channel_sum[channel_sum == 0] = 1e-6 
+
+    r_norm = r / channel_sum
+    g_norm = g / channel_sum
+    b_norm = b / channel_sum
+
+    norm_frame = cv2.merge([b_norm, g_norm, r_norm])
+    return (norm_frame * 255).astype(np.uint8)
+
+def updateModel(alpha: float = 0, new_roi_hist=None):
+    global roi_hist
+      
+    if alpha==0:
+        return
+    
+    roi_hist = cv2.addWeighted(roi_hist, (1 - alpha), new_roi_hist, alpha, 0)
+
+def computeRoiHist(hsv_roi, channels_str="H"):
+    channel_indices = []
+    channels_hist_sizes = []
+    channels_range = []
+
+    if "H" in channels_str:
+        channel_indices.append(0)
+        channels_hist_sizes.append(180)
+        channels_range.extend([0, 180])
+    if "S" in channels_str:
+        channel_indices.append(1)
+        channels_hist_sizes.append(256)
+        channels_range.extend([30, 256])
+    if "V" in channels_str:
+        channel_indices.append(2)
+        channels_hist_sizes.append(256)
+        channels_range.extend([20, 236])
+        
+    # Pixels with S<30, V<20 or V>235 are ignored 
+    mask = cv2.inRange(hsv_roi, np.array((0., 30., 20.)), np.array((180., 255., 235.)))
+    
+    roi_hist = cv2.calcHist([hsv_roi], channel_indices, mask, channels_hist_sizes, channels_range)
+    return cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
 
 #cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture('../Sequences/VOT-Ball.mp4')
@@ -49,19 +103,19 @@ cv2.setMouseCallback("First image", define_ROI)
  
 # keep looping until the 'q' key is pressed
 while True:
-	# display the image and wait for a keypress
-	cv2.imshow("First image", frame)
-	key = cv2.waitKey(1) & 0xFF
-	# if the ROI is defined, draw it!
-	if (roi_defined):
-		# draw a green rectangle around the region of interest
-		cv2.rectangle(frame, (r,c), (r+h,c+w), (0, 255, 0), 2)
-	# else reset the image...
-	else:
-		frame = clone.copy()
-	# if the 'q' key is pressed, break from the loop
-	if key == ord("q"):
-		break
+    # display the image and wait for a keypress
+    cv2.imshow("First image", frame)
+    key = cv2.waitKey(1) & 0xFF
+    # if the ROI is defined, draw it!
+    if (roi_defined):
+        # draw a green rectangle around the region of interest
+        cv2.rectangle(frame, (r,c), (r+h,c+w), (0, 255, 0), 2)
+    # else reset the image...
+    else:
+        frame = clone.copy()
+    # if the 'q' key is pressed, break from the loop
+    if key == ord("q"):
+        break
  
 track_window = (r,c,h,w)
 # set up the ROI for tracking
@@ -69,42 +123,32 @@ roi = frame[c:c+w, r:r+h]
 # conversion to Hue-Saturation-Value space
 # 0 < H < 180 ; 0 < S < 255 ; 0 < V < 255
 hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-# computation mask of the histogram:
-# Pixels with S<30, V<20 or V>235 are ignored 
-'''	
-	RGB colors are represented by 3D vector in color space (a cube)
-	The vertices of the cube are: black, red, green, blue, cyan, magenta, yellow, white
-	The color in the axis between black and white correspond to gray levels
-	Each color of the RGB cube can be decomposed in a Hue, Saturation and Value (HSV)
-	 - H: color type, i.e. angle of the chromatic component (0=red, 60=green, 120=blue)
-	 - S: purity of the color, distance from the gray axis (0=gray, 255=pure color)
-	 - V: brightness of the color (0=black, 255=white)
-	With the S mask defined below, we ignore pixels that are close to gray levels
-	With the V mask defined below, we ignore pixels that are too dark or too bright
-'''
-mask = cv2.inRange(hsv_roi, np.array((0.,30.,20.)), np.array((180.,255.,235.)))
-# Marginal histogram of the Hue component
-roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-# Histogram values are normalised to [0,255]
-cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+
+roi_hist = computeRoiHist(hsv_roi, channels_str=CHANNELS)
 
 # Setup the termination criteria: either 10 iterations,
 # or move by less than 1 pixel
 term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 
+
 cpt = 1
 while(1):
     ret ,frame = cap.read()
+    # frame = normalize_bgr_frame(frame)
     if ret == True:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         # Backproject the model histogram roi_hist onto the 
         # current image hsv, i.e. dst(x,y) = roi_hist(hsv(0,x,y))
-        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-		
+        # dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1) # hue
+        # dst = cv2.calcBackProject([hsv],[1],roi_hist,[0,256],1) # saturation 
+        dst = cv2.calcBackProject([hsv], [0, 1], roi_hist, [0, 180, 30, 256], 1)
+
         hue_rgb, sat, val, weights = show_channels_and_weigths(hsv, dst)
 
         # apply meanshift to dst to get the new location
         ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+
+        updateModel(0, computeRoiHist(hsv[c:c+w, r:r+h], channels_str=CHANNELS))
 
         # Draw a blue rectangle on the current image
         r,c,h,w = track_window
@@ -114,12 +158,12 @@ while(1):
         k = cv2.waitKey(60) & 0xff
         if k == 27:
             break
-        elif k == ord('s') or cpt in list(range(150, 161)):
-            cv2.imwrite('../Frames/Frame_%04d.png'%cpt,frame_tracked)
-            cv2.imwrite('../Frames/Hue_%04d.png'%cpt,hue_rgb)
-            cv2.imwrite('../Frames/Sat_%04d.png'%cpt,sat)
-            cv2.imwrite('../Frames/Val_%04d.png'%cpt,val)
-            cv2.imwrite('../Frames/Weights_%04d.png'%cpt,weights)
+        elif k == ord('s'):
+            cv2.imwrite(os.path.join(EXPERIMENT_PATH, 'Frame_%04d.png'%cpt),frame_tracked)
+            cv2.imwrite(os.path.join(EXPERIMENT_PATH, 'Hue_%04d.png'%cpt),hue_rgb)
+            cv2.imwrite(os.path.join(EXPERIMENT_PATH, 'Sat_%04d.png'%cpt),sat)
+            cv2.imwrite(os.path.join(EXPERIMENT_PATH, 'Val_%04d.png'%cpt),val)
+            cv2.imwrite(os.path.join(EXPERIMENT_PATH, 'Weights_%04d.png'%cpt),weights)
         cpt += 1
     else:
         break
